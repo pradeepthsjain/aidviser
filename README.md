f901b2aaddace360f3aa6d55fffe9e9059171358
pradeepthsjain <pradeepthsjain46@gmail.com>
2024-12-09 20:29:56 +0530

    folders created

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aidviser
+
+conda create -n aidviser python=3.10 -y
+
+conda activate aidviser
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Aidvise',
+    version= '0.0.0',
+    author= 'Pradeepth S Kumar',
+    author_email= 'pradeepthsjain46@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
